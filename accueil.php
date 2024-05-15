ddd43018fc1666ae16d3219d625f979c48f5c381
kilian <107520@ecole-it.com>
2024-05-15 22:58:05 +0200

    Initial commit

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="style_accueil.css" rel="stylesheet">
+    <title>Accueil</title>
+</head>
+<body>
+<a href="index.php">
+    <div class="navbar">
+        <p>Menu</p>
+    </div>
+</a>
+
+<h1>Bienvenue sur le site officiel de Bombit</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet">
+    <title>Homepage</title>
+</head>
+<body>
+
+<ol>
+    <li><a class="arrow" href="accueil.php">Accueil</a></li>
+    <li><a class="arrow">Règles</a></li>
+    <li><a class="arrow">Téléchargement</a></li>
+    <li><a class="arrow">Contacts</a></li>
+    <li><a class="arrow">Crédits</a></li>
+</ol>
+
+</body>
+</html>
