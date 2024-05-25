6fc10ebbc0ae4baeac6c15384ca6507816ab1ff5
enzopoussin <enzo.poussin@ecole-it.com>
2024-05-25 22:21:30 +0200

    avec des infos

--- accueil.php
+++ accueil.php
@@ -15,21 +15,21 @@
 <div class="acc">
     <h1>Bienvenue sur le site officiel de Bombit !</h1>
     <div class="pres">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Quis enim lobortis scelerisque fermentum dui faucibus. Volutpat est velit egestas dui. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Lobortis feugiat vivamus at augue. Facilisis mauris sit amet massa vitae tortor condimentum lacinia. Erat nam at lectus urna. Vitae justo eget magna fermentum iaculis eu non diam phasellus.</p>
+        <p>Bombit Man est un jeu rétro développé en 2024 par 2 étudiants en première année de l'École-IT qui sont Kilian Miliez et Enzo Poussin. Le jeu est développé en python en utilisant les librairies graphiques de pygame et de Tkinter. <br> Bombit Man reprend les caractéristiques du célébre jeu BomberMan. Préparez vous à jouer à un jeu explosif!</p>
     </div>
 
     <div class="card-container">
-        <a href="#">
+        <a href="#dl_contact">
             <div class="card">
                 <p>Téléchargement</p>
             </div>
         </a>
-        <a href="#">
+        <a href="#rule">
             <div class="card">
                 <p>Règles</p>
             </div>
         </a>
-        <a href="#">
+        <a href="#dl_contact">
             <div class="card">
                 <p>Contacts</p>
             </div>
@@ -40,8 +40,12 @@
 <div id="rule">
     <h2>RULES</h2>
     <div class="text">
-        <p id="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Quis enim lobortis scelerisque fermentum dui faucibus. Volutpat est velit egestas dui. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Lobortis feugiat vivamus at augue. Facilisis mauris sit amet massa vitae tortor condimentum lacinia. Erat nam at lectus urna. Vitae justo eget magna fermentum iaculis eu non diam phasellus.</p>
-        <p id="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Quis enim lobortis scelerisque fermentum dui faucibus. Volutpat est velit egestas dui. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Lobortis feugiat vivamus at augue. Facilisis mauris sit amet massa vitae tortor condimentum lacinia. Erat nam at lectus urna. Vitae justo eget magna fermentum iaculis eu non diam phasellus.</p>
+        <p id="left">1. Les murs: <br> Il existe 2 types de murs, des cassables et des incassables. Vous pouvez les casser en utilisant les bombes. <br><br> Les déplacements:
+            <br> Pour vous déplacer vous dévez utiliser les <strong>4 fléches directionnelles</strong> pour soit aller à gauche en bas vers le haut ou encore à droite.
+            <br><br> LES BOMBES: <br> Les bombes sont mortelles pour le joueur. Elles tueront le jour si il se situe dans son rayon d'éxplosion qui est de <strong>
+                2 blocs</strong> Vous pouvez une bombe en pressant la touche <strong>space</strong></p>
+        <p id="right">Les bombes exploseront 2 secondes après avoir étaient posées. <br><br> Les ennemies: <br> Si vous entrez en collision avec des ennemies vous perdez automatiquement. Vous pouvez tuer les énnemies avec vos bombes. Une fois tous les ennemies morts vous gagnez la partie.
+            <br><br> La map: <br> La map est générée automatiquement. Une seule instance du jeu n'est pas généree aléatoirement ce sont les murs incassables qui eux restes fixes.</p>
     </div>
 </div>
 
