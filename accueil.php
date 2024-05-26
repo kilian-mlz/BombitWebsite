ede225996faeec19adbc824af16c7b76a204a061
kilian <107520@ecole-it.com>
2024-05-26 11:30:14 +0200

    AVANCEMENT

--- accueil.php
+++ accueil.php
@@ -15,11 +15,11 @@
 <div class="acc">
     <h1>Bienvenue sur le site officiel de Bombit !</h1>
     <div class="pres">
-        <p>Bombit Man est un jeu rétro développé en 2024 par 2 étudiants en première année de l'École-IT qui sont Kilian Miliez et Enzo Poussin. Le jeu est développé en python en utilisant les librairies graphiques de pygame et de Tkinter. <br> Bombit Man reprend les caractéristiques du célébre jeu BomberMan. Préparez vous à jouer à un jeu explosif!</p>
+        <p>Bombit Man est un jeu rétro développé en 2024 par 2 étudiants en première année de l'École-IT qui sont Kilian Milliez et Enzo Poussin. Le jeu est développé en python en utilisant les librairies graphiques de pygame et de Tkinter. <br> Bombit Man reprend les caractéristiques du célébre jeu BomberMan. Préparez vous à jouer à un jeu explosif!</p>
     </div>
 
     <div class="card-container">
-        <a href="#dl_contact">
+        <a href="BOMBIT.zip">
             <div class="card">
                 <p>Téléchargement</p>
             </div>
@@ -42,11 +42,11 @@
     <div class="text">
         <p id="left">1. Les murs: <br> Il existe 2 types de murs, des cassables et des incassables. Vous pouvez les casser en utilisant les bombes. <br><br> Les déplacements:
             <br> Pour vous déplacer vous dévez utiliser les <strong>4 fléches directionnelles</strong> pour soit aller à gauche en bas vers le haut ou encore à droite.
-            <br><br> LES BOMBES: <br> Les bombes sont mortelles pour le joueur. Elles tueront le jour si il se situe dans son rayon d'éxplosion qui est de <strong>
-                2 blocs</strong> Vous pouvez une bombe en pressant la touche <strong>space</strong></p>
-        <p id="right">Les bombes exploseront 2 secondes après avoir étaient posées. <br><br> Les ennemies: <br> Si vous entrez en collision avec des ennemies vous perdez automatiquement. Vous pouvez tuer les énnemies avec vos bombes. Une fois tous les ennemies morts vous gagnez la partie.
-            <br><br> La map: <br> La map est générée automatiquement. Une seule instance du jeu n'est pas généree aléatoirement ce sont les murs incassables.
-            <br><br> Autre: <br> Si vous voulez quitter le jeu en cours de partie, appuyez sur <strong>ECHAPPE</strong></p>
+            <br><br> LES BOMBES: <br> Les bombes sont mortelles pour le joueur. Elles le tueront si il se situe dans son rayon d'explosion qui est de <strong>
+                2 blocs dans toutes les directions</strong> Vous pouvez placer une bombe en pressant la touche <strong>space</strong></p>
+        <p id="right">Les bombes exploseront 2 secondes après avoir étaient posées. <br><br> Les ennemies: <br> Si vous entrez en collision avec des ennemies vous perdez automatiquement. Vous pouvez tuer les ennemies avec vos bombes. Une fois tous les ennemies morts vous gagnez la partie.
+            <br><br> La map: <br> La map est générée automatiquement. Une seule instance du jeu n'est pas générée aléatoirement ce sont les murs incassables.
+            <br><br> Autre: <br> Si vous voulez quitter le jeu en cours de partie, appuyez sur <strong>ECHAP</strong></p>
     </div>
 </div>
 
